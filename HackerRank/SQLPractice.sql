4e1d986d73a3c95403e4ce2a0b3fd4d8aee0d651
Satya Chippada <janani.zalee@gmail.com>
2019-10-20 15:07:24 -0700

    Update SQLPractice.sql

--- HackerRank/SQLPractice.sql
+++ HackerRank/SQLPractice.sql
@@ -17,7 +17,7 @@ from occupations
 group by occupation
 order by count(occupation) asc, occupation asc;
                                            
-                                           /*
+----------------------------------------------------------
 """ 
 Write a query to find the node type of Binary Tree ordered by the value of the node. Output one of the following for each node:
 Root: If node is root node.
@@ -31,3 +31,30 @@ select n,
     end 
 from bst as b
 order by n;
+---------------------------------------------------------
+"""
+Given the table schemas below, write a query to print the company_code, founder name, total number of lead managers, total number of 
+senior managers, total number of managers, and total number of employees. Order your output by ascending company_code.
+The tables may contain duplicate records.
+The company_code is string, so the sorting should not be numeric. For example, if the company_codes are C_1, C_2, and C_10, then the 
+ascending company_codes will be C_1, C_10, and C_2.
+"""
+
+select c.company_code, c.founder, count(distinct e.lead_manager_code), count(distinct e.senior_manager_code), 
+count(distinct e.manager_code), count(distinct e.employee_code)
+from company c
+    inner join employee e
+    on c.company_code = e.company_code
+group by c.company_code, c.founder
+order by c.company_code;
+--------------------
+select c.company_code, c.founder, 
+    count(distinct l.lead_manager_code), count(distinct s.senior_manager_code), 
+    count(distinct m.manager_code),count(distinct e.employee_code) 
+from Company c, Lead_Manager l, Senior_Manager s, Manager m, Employee e 
+where c.company_code = l.company_code 
+    and l.lead_manager_code=s.lead_manager_code 
+    and s.senior_manager_code=m.senior_manager_code 
+    and m.manager_code=e.manager_code 
+group by c.company_code, c.founder
+order by c.company_code;
