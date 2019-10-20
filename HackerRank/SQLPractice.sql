22821291bc3c74fd88c2353557859a242326bec2
Satya Chippada <janani.zalee@gmail.com>
2019-10-20 14:19:55 -0700

    Update SQLPractice.sql

--- HackerRank/SQLPractice.sql
+++ HackerRank/SQLPractice.sql
@@ -16,3 +16,18 @@ select "There are a total of " + str(count(occupation)) + " " + lower(occupation
 from occupations
 group by occupation
 order by count(occupation) asc, occupation asc;
+                                           
+                                           /*
+""" 
+Write a query to find the node type of Binary Tree ordered by the value of the node. Output one of the following for each node:
+Root: If node is root node.
+Leaf: If node is leaf node.
+Inner: If node is neither root nor leaf node.                                           
+"""
+select n, 
+    case when p is null then 'Root'
+        when (select count(*) from bst where p=b.n)>0 then 'Inner'
+        else 'Leaf'
+    end 
+from bst as b
+order by n;
